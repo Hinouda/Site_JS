a0f36215161cbc03b2702b4bb926d17a0c7a5ad2
Daniel Nouchi <dnouchi72@gmail.com>
2022-11-09 19:41:09 -0300

    oi

--- main.js
+++ main.js
@@ -37,19 +37,19 @@ document.querySelector('#app').innerHTML =
             'SAO', 
             'Roubado',
             'https://cdn.myanimelist.net/images/anime/1993/93837.jpg',
-
+            'https://myanimelist.net/anime/36474/Sword_Art_Online__Alicization?q=swor&cat=anime'
         )}
         ${CreateAnime(
             'BangDream',
             'musica',
             'https://cdn.myanimelist.net/images/anime/1246/115689.jpg',
-
+            'https://myanimelist.net/anime/41781/BanG_Dream_Movie__Episode_of_Roselia_-_II__Song_I_Am?q=roselia&cat=anime'
         )}
          ${CreateAnime(
             'Chainsaw Man',
             'PRota Prota',
             'https://cdn.myanimelist.net/r/160x220/images/anime/1806/126216.webp?s=df8704391a49a3de82e6e0d59a755f20',
-
+            'https://myanimelist.net/anime/44511/Chainsaw_Man'
         )}
 
         ${CreateAnime(
