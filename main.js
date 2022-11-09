634e19cf82374054d7cd068b5c66cbcffa7115a8
Daniel Nouchi <dnouchi72@gmail.com>
2022-11-09 19:36:05 -0300

    oi

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-function CreateAnime(nome, descricao, img){
+function CreateAnime(nome, descricao, img, link){
     return `
 
-    <div class = "cards1">
+    <a href="${link}" class="cards1" target="_blank">
     <ul>
         <li>
-        <h2><strong> ${nome}</strong></h2>
+        <h2><strong> ${nome} </strong></h2>
         <p>Descrição</p>
         <h4> ${descricao}</h4>
         <div class = "BorderImg">
@@ -12,41 +12,51 @@ function CreateAnime(nome, descricao, img){
         </div>
         </li>
     </ul>
-    </div>
+    </a>
                 
     `
 }
 
 document.querySelector('#app').innerHTML = 
 `
+<style type="text/css">
+.cards1,.cards2,.cards3{
+    color: #ff0000;
+}
+</style>
     
         <main id = "cards">
        
         ${CreateAnime(
             'Rakudai kishi' ,
             'Anime Prota fraco/op',
-            'https://cdn.myanimelist.net/images/anime/9/76493.jpg'
+            'https://cdn.myanimelist.net/images/anime/9/76493.jpg',
+            'https://myanimelist.net/anime/30296/Rakudai_Kishi_no_Cavalry?q=hakudai&cat=anime'
         )}
         ${CreateAnime( 
             'SAO', 
             'Roubado',
-            'https://cdn.myanimelist.net/images/anime/1993/93837.jpg'
+            'https://cdn.myanimelist.net/images/anime/1993/93837.jpg',
+
         )}
         ${CreateAnime(
             'BangDream',
             'musica',
-            'https://cdn.myanimelist.net/images/anime/1246/115689.jpg'
+            'https://cdn.myanimelist.net/images/anime/1246/115689.jpg',
+
         )}
          ${CreateAnime(
             'Chainsaw Man',
             'PRota Prota',
-            'https://cdn.myanimelist.net/r/160x220/images/anime/1806/126216.webp?s=df8704391a49a3de82e6e0d59a755f20'
+            'https://cdn.myanimelist.net/r/160x220/images/anime/1806/126216.webp?s=df8704391a49a3de82e6e0d59a755f20',
+
         )}
 
         ${CreateAnime(
             'Chainsaw Man',
             'PRota Prota',
-            'https://cdn.myanimelist.net/r/160x220/images/anime/1806/126216.webp?s=df8704391a49a3de82e6e0d59a755f20'
+            'https://cdn.myanimelist.net/r/160x220/images/anime/1806/126216.webp?s=df8704391a49a3de82e6e0d59a755f20',
+
         )}
        
         </main>
